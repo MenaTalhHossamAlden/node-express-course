065c3f1165f37073e10c0d7f41008576aeb929ac
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-09-15 20:39:12 +0300

    Modules

---
+++ 1-node-tutorial/3-modules.js
@@ -0,0 +1,9 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+const names = require('./4-names.js');
+const sayHi = require('./5-utils.js');
+const data = require('./6-alternative-flavor.js');
+console.log(data);
+sayHi('susan');
+sayHi(names.john);
+sayHi(names.peter);
