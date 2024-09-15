ca9f1be5e86387ec3a074a22dd1b8b3137fed3bb
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-09-15 21:11:00 +0300

    Mind Grenade

--- 1-node-tutorial/3-modules.js
+++ 1-node-tutorial/3-modules.js
@@ -3,6 +3,7 @@
 const names = require('./4-names.js');
 const sayHi = require('./5-utils.js');
 const data = require('./6-alternative-flavor.js');
+require('./7-mind-grenade.js');
 console.log(data);
 sayHi('susan');
 sayHi(names.john);
